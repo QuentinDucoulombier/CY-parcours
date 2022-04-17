c6b990d53477ce135cfdb9a380e2f816e874b876
Quentin <quentin@LAPTOP-QUP4NKFQ.localdomain>
2022-04-17 20:01:13 +0200

    ajout de 2 pauvres trucs parce que jsuis un mec serieux

--- code/enregistrerEleve.php
+++ code/enregistrerEleve.php
@@ -1,34 +1,41 @@
 <?php
     session_start();
-    if (($handle = fopen("../data/choixEtudiantsParcours1.csv", "r")) !== FALSE) { //TODO : etendre au autre fichier (choixEtudiantsParcours2 et choixEtudiantsParcours3)
-        while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-    
-    
-            if (($data[2] == $_POST["email"]) && ($data[1] == $_POST["nom"])){  //On verifie si son adresse mail et son nom existe dans la base de donnée
-               
-                if(!file_exists("../data/loginEleves.csv"))
-                {
-                    $file = fopen("../data/loginEleves.csv","w");
-                    $listeTitre = array("Prenom","Nom","Email","pseudo","mdp");
-                    fputcsv($file, $listeTitre, ";");
-                    fclose("../data/loginEleves.csv");
-    
-                }
+    function enregistre($fichier)
+    {
+        if (($handle = fopen($fichier, "r")) !== FALSE) { //TODO : etendre au autre fichier (choixEtudiantsParcours2 et choixEtudiantsParcours3)
+            while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+        
+        
+                if (($data[2] == $_POST["email"]) && ($data[1] == $_POST["nom"])){  //On verifie si son adresse mail et son nom existe dans la base de donnée
                 
-                //on importe les infos de l'inscription dans un csv
-                $file = fopen("../data/loginEleves.csv","a");
-                $list = array($_POST['prenom'], $_POST['nom'], $_POST['email'], $_POST['pseudo'], $_POST['password']);
-                fputcsv($file, $list, ";");
-                fclose($file);
-                header('Location: connexion.php');
-                exit();
+                    if(!file_exists("../data/loginEleves.csv"))
+                    {
+                        $file = fopen("../data/loginEleves.csv","w");
+                        $listeTitre = array("Prenom","Nom","Email","pseudo","mdp");
+                        fputcsv($file, $listeTitre, ";");
+                        fclose("../data/loginEleves.csv");
+        
+                    }
+                    
+                    //on importe les infos de l'inscription dans un csv
+                    $file = fopen("../data/loginEleves.csv","a");
+                    $list = array($_POST['prenom'], $_POST['nom'], $_POST['email'], $_POST['pseudo'], $_POST['password']);
+                    fputcsv($file, $list, ";");
+                    fclose($file);
+                    header('Location: connexion.php');
+                    exit();
+                }
+                else
+                {
+                header('Location: inscription.html'); //TODO mettre un msg d'erreur (en ajax en gros)
+                }
             }
-            else
-            {
-            header('Location: inscription.html'); //TODO mettre un msg d'erreur (en ajax en gros)
-            }
+            fclose($handle);
         }
-        fclose($handle);
     }
+    enregistre("../data/choixEtudiantsParcours1.csv");
+    enregistre("../data/choixEtudiantsParcours2.csv");
+    enregistre("../data/choixEtudiantsParcours3.csv");
+
 ?>
 
