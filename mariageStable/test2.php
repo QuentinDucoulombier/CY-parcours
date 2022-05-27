a70b31f47d69a55d69f17cfdfb196d2048058ac4
Quentin <quentin@LAPTOP-QUP4NKFQ.localdomain>
2022-05-27 22:34:16 +0200

    ajout de luthuluthulu work

---
+++ mariageStable/test2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Plus test que ca tu meurs</title>
+    <script src="zebi.js" charset="utf-8"></script>
+  </head>
+  <body onload="doMarriage(1)">
+  </body>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js" integrity="sha512-SGWgwwRA8xZgEoKiex3UubkSkV1zSE1BS6O4pXcaxcNtUlQsOmOmhVnDwIvqGRfEmuz83tIGL13cXMZn6upPyg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</html>
--- test/frr.js
+++
@@ -1,5 +0,0 @@
-const fileInput = document.getElementById('input');
-fileInput.onchange = () => {
-  const selectedFile = fileInput.files[0];
-  console.log(selectedFile);
-}
