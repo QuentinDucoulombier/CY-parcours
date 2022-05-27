10c217876f90e9283b38d3669afa82772b23ddfd
Lucas <thupingone@LAPTOP-LDRDT03Q.localdomain>
2022-05-27 22:48:33 +0200

    FUCK

--- mariageStable/mariage.php
+++ mariageStable/mariage.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Plus test que ca tu meurs</title>
-    <script src="zebi.js" charset="utf-8"></script>
+    <script src="stable.js" charset="utf-8"></script>
   </head>
   <body onload="doMarriage(1)">
   </body>
