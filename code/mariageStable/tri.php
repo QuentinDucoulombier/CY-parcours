aa2f3ecdb085a1c340845a49ecb0776ba9d7be85
Noé <noe@DESKTOP-O55IVOE.localdomain>
2022-05-28 19:10:07 +0200

    fdp

---
+++ code/mariageStable/tri.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Plus test que ca tu meurs</title>
+    <script src="tri.js" charset="utf-8"></script>
+  </head>
+  <body onload="tri('../../data/choixEtudiantsParcours1.csv')">
+  </body>
+</html>
